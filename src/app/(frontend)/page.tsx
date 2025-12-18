5c71c8787f3a42ccbd5e5288371b728c42218b47
Luc Pierrot Dumas <112646178+luc-dumas@users.noreply.github.com>
2025-12-18 09:38:37 -0500

    Checking authentication on Next.js frontend

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,19 @@
-import PageTemplate, { generateMetadata } from './[slug]/page'
+// import PageTemplate, { generateMetadata } from './[slug]/page'
 
-export default PageTemplate
+// export default PageTemplate
 
-export { generateMetadata }
+// export { generateMetadata }
+import {headers as getHeaders} from 'next/headers'
+import {getPayload} from 'payload'
+import config from '@/payload.config'
+
+export default async function Home() {
+  const headers = await getHeaders()
+  const payload = await getPayload({config})
+  const {permissions, user} = await payload.auth({headers})
+
+  return <h1>
+    {user ? `You are authenticated as ${user.roles?.map(role => role).join(', ')}` : 'You are not logged in'}
+  </h1>
+
+}
